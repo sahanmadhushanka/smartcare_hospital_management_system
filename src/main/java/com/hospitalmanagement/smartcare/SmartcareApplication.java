2e91a1165d6cd83d24d7c5207a051e630c0688c0
Sahan Madhushanka <sahanmadhushanka679@gmail.com>
2026-08-04 21:56:57 +0530

    Add files via upload

---
+++ src/main/java/com/hospitalmanagement/smartcare/SmartcareApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.smartcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcareApplication.class, args);
+	}
+
+}
